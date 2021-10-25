03d72032d2863d3e894b680c361f8173526fa5a9
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-10-25 19:12:02 -0400

    Style About me page

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,8 +4,8 @@ import { Container, Row, Col } from "reactstrap";
 function Content(props) {
 
     return(
-        <Container fluid={true}>
-            <Row className="justify-content-center">
+        <Container fluid={true} className="rounded mx-auto d-block">
+            <Row className="justify-content-center ">
                 <Col md={5}>
                     {props.children}
                 </Col>
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Content from '../components/Content';
 import Hero from '../components/Hero';
-import { Container, Row, Col } from "reactstrap";
 import Image from 'react-bootstrap/Image'
 
 import headshot from '../assets/images/headshot.jpg'
@@ -10,28 +9,23 @@ function AboutPage(props) {
     
     return(
         <div>
-            <Hero title={props.title} />
-            
 
-            <Container fluid={true}>
-                <Row className="justify-content-center py-5">
-                    <Col xs={6} md={4}>
-                    <Image src={headshot} alt="Brendan" rounded />
-                    </Col> 
-                </Row>
-            </Container>
+            <Image className="rounded mx-auto d-block mt-5" src={headshot} alt="Brendan" rounded />
+                  
+
 
             <Content>
-            <p>As a skilled tradesman laid off during the height of the COVID crisis I decided to focus on retraining and setting myself on a new path. I took a risk and joined the Penn LPS Coding Bootcamp.</p>
+                    <Hero title={props.title} />
+                        
+                    <p>As a skilled tradesman laid off during the height of the COVID crisis I decided to focus on retraining and setting myself on a new path. I took a risk and joined the Penn LPS Coding Bootcamp.
+                    
+                    Now as a full stack web developer I am continually striving to evolve what I know, write cleaner code, and learn new languages and skills to improve my abilities and add to my toolbox.</p>
 
-            <p>Now as a full stack web developer I am continually striving to evolve what I know, write cleaner code, and learn new languages and skills to improve my abilities and add to my toolbox.</p>
-
-            <p>Versed in HTML, CSS, Javascript, Bootstrap, JQuery, Node, and MYSQL. As well as progressive web apps, agile methodology, and database theory.</p>
-            
-            <p>Here you will find several small projects that showcase proficiency in a variety of areas including the above.</p>
-
-            
+                    <p>Versed in HTML, CSS, Javascript, Bootstrap, JQuery, Node, and MYSQL. As well as progressive web apps, agile methodology, and database theory.</p>
+                    
+                    <p>Here you will find several small projects that showcase proficiency in a variety of areas including the above.</p>
             </Content>
+          
         </div>
     );
 
