ca7b4527d460f8342403df0ede92288899244ac8
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-10-29 14:19:50 -0400

    reset the home route

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ constructor(props) {
   this.state = {
     title: 'Brendan Rottmund',
     headerLinks: [
-      { title: 'Home', path: '/home'},
+      { title: 'Home', path: '/'},
       { title: 'About', path: '/about'},
       { title: 'Contact', path: '/contact'},
     ],
@@ -48,14 +48,14 @@ constructor(props) {
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
-                <Link className="nav-link" to="/home">Home</Link>
+                <Link className="nav-link" to="/">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
               <Switch>
-                <Route exact path="/home" render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+                <Route exact path="/" render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
               </Switch>  
               <Switch>
                 <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
