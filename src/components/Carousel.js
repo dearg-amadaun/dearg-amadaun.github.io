904e7d496a8967b4b2a0fca5f7e7a97d0ef9a3ce
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-11-01 16:10:48 -0400

    landing page projects

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,12 @@ import { Container, Row } from 'reactstrap';
 import Card from '../components/Card';
 
 //imgs
-import fitnesstracker from '../assets/images/fitlist.jpg'
+import quiz from '../assets/images/large/quiz.jpg'
+import fitlist from '../assets/images/fitlist.jpg'
+import fitnesstracker from '../assets/images/large/fitnesstracker.png'
+import passgen from '../assets/images/large/passgen.jpg'
+import note from '../assets/images/large/note.jpg'
+import readmeGen from '../assets/images/large/readmeGen.jpg'
 
 class Carousel extends React.Component {
 
@@ -13,18 +18,18 @@ class Carousel extends React.Component {
             items: [
                 {
                     id:0,
-                    title:'Fitness Tracker',
-                    subTitle:'A simple mongoDb based workout tracker.',
-                    imgSrc: fitnesstracker,
-                    link:'https://calm-harbor-58560.herokuapp.com/',
+                    title:'Quizzes and Cocktails',
+                    subTitle:'A social app that generates random cocktails and trivia.',
+                    imgSrc: quiz,
+                    link:'https://sf487552.github.io/Group-Project-1/',
                     selected: false
                 },
                 {
                     id:1,
-                    title:'Fitness Tracker',
-                    subTitle:'A simple mongoDb based workout tracker.',
-                    imgSrc: fitnesstracker,
-                    link:'https://calm-harbor-58560.herokuapp.com/',
+                    title:'Fitlist',
+                    subTitle:'An early attempt at building a profile based workout buuilder.',
+                    imgSrc: fitlist,
+                    link:'https://obscure-falls-14224.herokuapp.com/',
                     selected: false
                 },
                 {
@@ -37,28 +42,28 @@ class Carousel extends React.Component {
                 },
                 {
                     id:3,
-                    title:'Fitness Tracker',
-                    subTitle:'A simple mongoDb based workout tracker.',
-                    imgSrc: fitnesstracker,
-                    link:'https://calm-harbor-58560.herokuapp.com/',
+                    title:'Password Generator',
+                    subTitle:'A javascript random password generator.',
+                    imgSrc: passgen,
+                    link:'https://dearg-amadaun.github.io/Password-Generator/',
                     selected: false
                 },
                 {
                     id:4,
-                    title:'Fitness Tracker',
-                    subTitle:'A simple mongoDb based workout tracker.',
-                    imgSrc: fitnesstracker,
-                    link:'https://calm-harbor-58560.herokuapp.com/',
+                    title:'Note Taker',
+                    subTitle:'An express based app for writing and saving notes.',
+                    imgSrc: note,
+                    link:'https://note-taker-express-homework-11.herokuapp.com/',
                     selected: false
                 },
                 {
                     id:5,
-                    title:'Fitness Tracker',
-                    subTitle:'A simple mongoDb based workout tracker.',
-                    imgSrc: fitnesstracker,
-                    link:'https://calm-harbor-58560.herokuapp.com/',
+                    title:'Readme Generator',
+                    subTitle:'A node.js app that uses basic command line inputs to generate a markdown README file.',
+                    imgSrc: readmeGen,
+                    link:'https://github.com/dearg-amadaun/Readme-Generator',
                     selected: false
-                },
+                }
 
             ]
     
